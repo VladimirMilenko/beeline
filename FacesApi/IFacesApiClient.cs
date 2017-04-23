4690943f209417df5a7f3e1e0ecef36046243be5
Vladimir Milenko <v@milenko.pro>
2017-04-23 03:51:29 +0300

    IFaces api

---
+++ FacesApi/IFacesApiClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacesApi
+{
+    public interface IFacesApiClient
+    {
+        Task<OperationResult> CreatePerson(string name, string groupId);
+        Task<OperationResult> AddPersonFace(string groupId, string personId, byte[] imageBytes);
+        Task<OperationResult> DetectFace(byte[] imageBytes);
+        Task<OperationResult> Identify(string groupId, string personId);
+        Task<OperationResult> TrainGroup(string groupId);
+    }
+}
