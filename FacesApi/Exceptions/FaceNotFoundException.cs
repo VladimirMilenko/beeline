792c8c0300c716ed336a095032ac42d9ea59cb89
Vladimir Milenko <v@milenko.pro>
2017-04-23 03:57:55 +0300

    Faces Exceptions

---
+++ FacesApi/Exceptions/FaceNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacesApi.Exceptions
+{
+    public class FaceNotFoundException : Exception
+    {
+        public FaceNotFoundException()
+             : base("Face not found")
+        {
+        }
+        public FaceNotFoundException(string message)
+             : base("Face not found")
+        {
+        }
+        public FaceNotFoundException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
---
+++ FacesApi/Exceptions/FacesApiException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacesApi.Exceptions
+{
+    public class FacesApiException : Exception
+    {
+        public FacesApiException()
+             : base("Something went wrong")
+        {
+        }
+        public FacesApiException(string message)
+             : base("Something went wrong")
+        {
+        }
+        public FacesApiException(string message, Exception inner)
+        : base(message, inner)
+        {
+        }
+    }
+}
